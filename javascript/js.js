78e8a825fcc7239e6b831254c359d76a111812ab
ChrMottola <chrmottola@hotmail.com>
2022-09-28 14:24:19 -0300

    Actualizacion JavaScript

---
+++ javascript/js.js
@@ -0,0 +1,30 @@
+console.log("hola mundo desde archivo externo");
+// Variables
+
+//String cadena (texto) de caracteres, se usa ""
+let nombre= "Christian";
+let apellido="Mottola";
+// Variable booleana "si o no" true/false
+let mascotas="true";
+// Numericas sin ""
+let edad=45;
+//Operaciones con variables
+let numeroa=12;
+let numerob=3
+
+let resultadosuma=numeroa+numerob;
+let resultadomultiplicacion=numeroa*numerob;
+
+
+console.log(nombre);
+console.log(apellido);
+console.log(mascotas);
+console.log(edad);
+
+console.log(resultadosuma);
+console.log(resultadomultiplicacion);
+
+let nombreIngresado= prompt("Ingrese Nombre");
+let mensajeSalida= "Su Nombre es " +" "+ nombreIngresado;
+alert(mensajeSalida)
+console.log(mensajeSalida)
